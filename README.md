6deb42e05a5a26c22a007f3baf69e49694176d1f
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-07-27 16:59:23 +0000

    Assume secrets are already on machine

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Simpleinfra
 retart-rcs
 ----------
 
-rcs data and secrets are assumed to be in a directory called `rcs-data` within the current
-directory. The data will be synced to `/opt/rcs/data` on the target machine.
+rcs data/secrets should be in /opt/rcs/data on the target machine
--- restart-rcs.sh
+++ restart-rcs.sh
@@ -2,31 +2,20 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-RCS_DATA=$(pwd)/rcs-data
 TARGET_BOX=rcs.rust-lang.org
 RCS_BOX_USER=$USER
 
-if [ ! -d "$RCS_DATA" ]; then
-	echo "No rcs dir '$RCS_DATA'"
-	exit 1
-fi
-
-ssh $RCS_BOX_USER@$TARGET_BOX '
-	sudo mkdir -p /opt/rcs &&
-	chown $USER: /opt/rcs
-'
 rsync -avr --delete rcs-data/ $RCS_BOX_USER@$TARGET_BOX:/opt/rcs/data/
 ssh $RCS_BOX_USER@$TARGET_BOX '
-	set -o errexit &&
-	set -o pipefail &&
-	set -o nounset &&
-	sudo mkdir -p /opt/rcs &&
-	chown $USER: /opt/rcs &&
-	cd /opt/rcs &&
-	docker pull alexcrichton/rust-central-station &&
-	(docker rm -f rcs || true) &&
-	docker run \
-		--name rcs \
+    set -o errexit &&
+    set -o pipefail &&
+    set -o nounset &&
+    cd /opt/rcs &&
+    test -d data &&
+    docker pull alexcrichton/rust-central-station &&
+    (docker rm -f rcs || true) &&
+    docker run \
+        --name rcs \
         --volume `pwd`/data:/data \
         --volume `pwd`/data/letsencrypt:/etc/letsencrypt \
         --volume `pwd`/logs:/var/log \
