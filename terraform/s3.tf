f1a762c16cbb9ee63bc06df94d6e1b555c699df6
Pietro Albini <pietro@pietroalbini.org>
2019-10-28 13:40:20 +0100

    terraform: fix formatting

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -55,9 +55,9 @@ EOF
 resource "aws_s3_bucket_public_access_block" "rust_inventories" {
   bucket = aws_s3_bucket.rust_inventories.id
 
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
