37166e820b3a728e2ddbb4e1a3e4355af8588104
Pietro Albini <pietro@pietroalbini.org>
2019-11-06 10:34:19 +0100

    terraform: remove custom cache ttl for static.crates.io

--- terraform/services/cratesio/cloudfront-static.tf
+++ terraform/services/cratesio/cloudfront-static.tf
@@ -22,40 +22,6 @@ resource "aws_cloudfront_distribution" "static" {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
 
-    // Override any cache-related header, and cache for 10 years Most of the
-    // content on static.crates.io is static (doh), so this is safe to do. For
-    // content that change over time there are other cache behaviors below that
-    // override this.
-    default_ttl = 315360000
-    min_ttl     = 315360000
-    max_ttl     = 315360000
-
-    forwarded_values {
-      headers      = []
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  ordered_cache_behavior {
-    path_pattern           = "/db-dump.tar.gz"
-    target_origin_id       = "main"
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-
-    // Override any cache-related header, and never cache this file. Database
-    // dumps are updated daily, and caching them might result in stale data
-    // being returned. They're usually requested by batch jobs on other people'
-    // servers so disabling CloudFront edge caching is not going to affect them
-    // that much.
-    default_ttl = 0
-    min_ttl     = 0
-    max_ttl     = 0
-
     forwarded_values {
       headers      = []
       query_string = false
