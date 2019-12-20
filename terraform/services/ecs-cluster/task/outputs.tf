c9af8cb0491f9bb6063545584765ed67881437d5
Pietro Albini <pietro@pietroalbini.org>
2019-12-20 18:16:46 +0100

    terraform: fmt

--- terraform/services/ecs-cluster/task/outputs.tf
+++ terraform/services/ecs-cluster/task/outputs.tf
@@ -1,4 +1,4 @@
 output "arn" {
   description = "The ARN of the task definition created by this module."
-  value = aws_ecs_task_definition.task.arn
+  value       = aws_ecs_task_definition.task.arn
 }
