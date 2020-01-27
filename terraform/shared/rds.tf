8f1a50f0ccdf9e2188ad2a526095141c9e55edb5
Mark Rousskov <mark.simulacrum@gmail.com>
2020-01-27 11:43:46 -0500

    Add RDS database

---
+++ terraform/shared/rds.tf
@@ -0,0 +1,24 @@
+resource "random_password" "database" {
+  length = 25
+  special = true
+  override_special = "_%@"
+}
+
+resource "aws_db_subnet_group" "public" {
+  name       = "public"
+  subnet_ids = module.vpc_prod.public_subnets
+
+  tags = {
+    Name = "primary db subnet"
+  }
+}
+resource "aws_db_instance" "primary" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "11.5"
+  instance_class       = "db.t3.micro"
+  username             = "root"
+  password             = random_password.database.result
+  db_subnet_group_name = aws_db_subnet_group.public.name
+}
