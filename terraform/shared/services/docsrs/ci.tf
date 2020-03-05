9403dd86068281f9cbe20967f1200a0e65805566
Pietro Albini <pietro@pietroalbini.org>
2020-03-05 15:08:44 +0100

    terraform.shared: move docs-rs ecr repo inside the service

--- terraform/shared/services/docsrs/ci.tf
+++ terraform/shared/services/docsrs/ci.tf
@@ -1,5 +1,10 @@
 // This file contains the configuration for the rust-lang/crater's CI.
 
+module "ecr" {
+  source = "../../modules/ecr-repo"
+  name   = "docsrs"
+}
+
 resource "aws_iam_user" "ci" {
   name = "ci--rust-lang--docs-rs"
 }
@@ -10,10 +15,10 @@ resource "aws_iam_access_key" "ci" {
 
 resource "aws_iam_user_policy_attachment" "ci_push" {
   user       = aws_iam_user.ci.name
-  policy_arn = var.ecr_repo.policy_push_arn
+  policy_arn = module.ecr.policy_push_arn
 }
 
 resource "aws_iam_user_policy_attachment" "ci_pull" {
   user       = aws_iam_user.ci.name
-  policy_arn = var.ecr_repo.policy_pull_arn
+  policy_arn = module.ecr.policy_pull_arn
 }
