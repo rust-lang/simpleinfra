84bf1d26a3a6c296769fa1b946e57eb132c83929
Pietro Albini <pietro@pietroalbini.org>
2020-03-05 15:58:13 +0100

    terraform.docs-rs: import cloudfront

---
+++ terraform/docs-rs/cloudfront.tf
@@ -0,0 +1,110 @@
+// This file configures the docs.rs CloudFront distribution.
+
+locals {
+  domain_name    = "docs.rs"
+  domain_is_apex = true
+  origin         = "docsrs.infra.rust-lang.org"
+}
+
+module "certificate" {
+  source = "../shared/modules/acm-certificate"
+  providers = {
+    aws = aws.east1
+  }
+
+  domains = [
+    local.domain_name,
+  ]
+}
+
+resource "aws_cloudfront_distribution" "webapp" {
+  comment = local.domain_name
+
+  enabled             = true
+  wait_for_deployment = false
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_All"
+
+  aliases = [local.domain_name]
+  viewer_certificate {
+    acm_certificate_arn      = module.certificate.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "ec2"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 31536000 // 1 year
+
+    forwarded_values {
+      headers = [
+        // Allow detecting HTTPS from the webapp
+        "CloudFront-Forwarded-Proto",
+        // Allow detecting the domain name from the webapp
+        "Host",
+      ]
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  origin {
+    origin_id   = "ec2"
+    domain_name = local.origin
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    TeamAccess = "docs-rs"
+  }
+}
+
+resource "aws_route53_record" "webapp" {
+  for_each = toset(local.domain_is_apex ? [] : [""])
+
+  zone_id = data.aws_route53_zone.webapp.id
+  name    = local.domain_name
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_cloudfront_distribution.webapp.domain_name]
+}
+
+data "aws_route53_zone" "webapp" {
+  // Convert foo.bar.baz into bar.baz
+  name = join(".", reverse(slice(reverse(split(".", local.domain_name)), 0, 2)))
+}
+
+resource "aws_route53_record" "webapp_apex" {
+  for_each = toset(local.domain_is_apex ? ["A", "AAAA"] : [])
+
+  zone_id = data.aws_route53_zone.webapp.id
+  name    = local.domain_name
+  type    = each.value
+
+  alias {
+    name                   = aws_cloudfront_distribution.webapp.domain_name
+    zone_id                = aws_cloudfront_distribution.webapp.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
