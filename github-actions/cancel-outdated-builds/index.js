96bf6b8488f6648c1819838c0a9d9847eaae130f
Pietro Albini <pietro@pietroalbini.org>
2020-03-19 14:40:00 +0100

    cancel-outdated-builds: use the new actions API to cancel builds

--- github-actions/cancel-outdated-builds/index.js
+++ github-actions/cancel-outdated-builds/index.js
@@ -1,20 +1,12 @@
 const actions = require("@actions/core");
 const childProcess = require("child_process");
 const fetch = require("node-fetch");
-const psList = require("ps-list");
-
-// Name of the process that will be killed when the build is outdated. On
-// Windows `.exe` will be appended to the name.
-const WORKER_NAME = "Runner.Worker";
 
 const main = (async () => {
     let checkEverySeconds = parseInt(actions.getInput("check_every_seconds", { required: true }));
     while (true) {
         if (await isLatestCommit() === false) {
-            let pid = await findWorkerPid();
-            if (pid !== null) {
-                process.kill(pid, 'SIGTERM');
-            }
+            await cancelRun();
             return;
         }
         await new Promise(resolve => setTimeout(resolve, checkEverySeconds * 1000));
@@ -57,13 +49,27 @@ const isLatestCommit = async () => {
     }
 };
 
-const daemonize = async () => {
-    let workerPid = await findWorkerPid();
-    if (workerPid === null) {
-        actions.setFailed("A process named " + WORKER_NAME + " is not running!");
-        return;
-    }
+const cancelRun = async () => {
+    let repo = process.env["GITHUB_REPOSITORY"];
+    let run_id = process.env["GITHUB_RUN_ID"];
+    let token = actions.getInput("github_token", { required: true });
 
+    let resp = await fetch("https://api.github.com/repos/" + repo + "/actions/runs/" + run_id + "/cancel", {
+        method: "POST",
+        headers: {
+            "Authorization": "token " + token,
+        },
+    });
+
+    if (resp.status !== 202) {
+        actions.setFailed(
+            "Received unexpected status code " + resp.status + "while cancelling the build!"
+        );
+        process.exit(1);
+    }
+};
+
+const daemonize = async () => {
     let subprocess = childProcess.fork(__filename, ["foreground"], {
         detached: true,
         stdio: "ignore",
@@ -72,24 +78,6 @@ const daemonize = async () => {
 
     actions.info("Successfully daemonized cancel-outdated-builds.");
     process.exit(0);
-};
-
-const findWorkerPid = async () => {
-    let procName = binaryName(WORKER_NAME);
-    let proc = (await psList()).find(proc => proc.name === procName);
-    if (proc === undefined) {
-        return null;
-    } else {
-        return proc.pid;
-    }
-}
-
-const binaryName = name => {
-    if (process.platform === "win32") {
-        return name + ".exe";
-    } else {
-        return name;
-    }
 };
 
 if (process.argv.length == 3 && process.argv[2] == "foreground") {
