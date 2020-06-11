12a8e9f16a6bf0c26055f0085e23afa04cd9390f
Pietro Albini <pietro@pietroalbini.org>
2020-06-11 17:59:46 +0200

    ansible: only rebuild images in gha-self-hosted when necessary

--- ansible/roles/gha-self-hosted/templates/update-source.sh
+++ ansible/roles/gha-self-hosted/templates/update-source.sh
@@ -7,6 +7,13 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cd /opt/gha-self-hosted/source
-git pull
 
-sudo systemctl start gha-self-hosted-rebuild-image.service
+start_sha="$(git rev-parse HEAD)"
+git pull
+end_sha="$(git rev-parse HEAD)"
+
+# If there were changes in the images/ directory between the two commits,
+# rebuild the local images.
+if git diff "${start_sha}..${end_sha}" --name-only | grep "^images/" >/dev/null; then
+    sudo systemctl start gha-self-hosted-rebuild-image.service
+fi
