c95396c9698ab563b466e7eaebf06ca1b9103c59
Pietro Albini <pietro@pietroalbini.org>
2020-08-11 11:12:56 +0200

    terraform: add dmarc record to crates.io

--- terraform/dns/crates.io.tf
+++ terraform/dns/crates.io.tf
@@ -26,5 +26,6 @@ module "crates_io" {
     "mail"             = [local.mailgun_spf],
     "k1._domainkey"    = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+eQtAm5xohRQJCbwTQ8e27GhOdxji94CVOBXQtuiq75sEVu9nMImBiv8UOipvmiTFsM4Odnp7mIq8UeyPDpeUwtbPv9SYflEJt6o+Im5YOU/UnhEXqa1tXbpFrQSISVYz129G1SxGKMmxSBvyrjPNBJJOofqLHrIQrrdcgh5CywIDAQAB"],
     "mailo._domainkey" = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDu3zVBL/h7U1maoWkGJMi5Gd6OGhqsuBt19o3cRrkaxI1+XMJw9GIPXSc9BZZruVOICJ2Y1SJI2A2SPGIIa9CqYhlyPEcL5AvtoaORX+VjlsPXcdoUH4eX5fjjrWRn8PlWqBrILTRVkhMrz6luFMmzmmHpHC0WbwIQ7QZgnexn5wIDAQAB"],
+    "_dmarc"           = ["v=DMARC1; p=none; rua=mailto:dmarc-rua@rust-lang.org; fo=1; ruf=mailto:dmarc-rua@rust-lang.org"],
   }
 }
