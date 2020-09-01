c113b135da99b6e0dcd6726b6c38b23a4d9dd84d
Pietro Albini <pietro@pietroalbini.org>
2020-09-01 15:04:03 +0200

    terraform: fix fmt

--- terraform/crater/main.tf
+++ terraform/crater/main.tf
@@ -17,7 +17,7 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 2.70"
     }
   }
--- terraform/docs-rs/_terraform.tf
+++ terraform/docs-rs/_terraform.tf
@@ -5,7 +5,7 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 2.70"
     }
   }
