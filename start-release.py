1564771c712a513a88a184a0fbe17df6855c8f3b
Pietro Albini <pietro@pietroalbini.org>
2020-10-15 11:17:47 +0200

    start-release: improve its output

--- start-release.py
+++ start-release.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import subprocess
 import sys
 import tempfile
+import time
+
 
 ALLOWED_ENVIRONMENTS = ["dev", "prod"]
 ALLOWED_CHANNELS = ["nightly", "beta", "stable"]
+
+# Environment variables to set in the CodeBuild runner if they're present in
+# the local system too.
+FORWARD_ENVIRONMENT_VARIABLES = [
+    "PROMOTE_RELEASE_ALLOW_MULTIPLE_TODAY",
+]
 
 
 def main(env, channel, override_commit):
@@ -23,8 +32,11 @@ def main(env, channel, override_commit):
     vars["PROMOTE_RELEASE_CHANNEL"] = channel
     if override_commit is not None:
         vars["PROMOTE_RELEASE_OVERRIDE_COMMIT"] = override_commit
+    for key, value in os.environ.items():
+        if key in FORWARD_ENVIRONMENT_VARIABLES:
+            vars[key] = value
 
-    subprocess.run([
+    res = subprocess.run([
         "aws", "codebuild", "start-build",
         "--project-name", f"promote-release--{env}",
         "--environment-variables-override", json.dumps([
@@ -35,7 +47,28 @@ def main(env, channel, override_commit):
             }
             for name, value in vars.items()
         ]),
-    ], check=True)
+    ], stdout=subprocess.PIPE, check=True)
+    build = json.loads(res.stdout)["build"]
+    print(f"Build started with ID '{build['id']}'")
+
+    build = {"id": "promote-release--dev:1dacf12e-1919-4ed6-9dfb-e357749eac6d", "logs": {}}
+
+    # If the CloudWatch stream was not created yet poll the API
+    while "streamName" not in build["logs"]:
+        time.sleep(1)
+
+        res = subprocess.run([
+            "aws", "codebuild", "batch-get-builds",
+            "--ids", build["id"],
+        ], stdout=subprocess.PIPE, check=True)
+        build = json.loads(res.stdout)["builds"][0]
+
+    print()
+    print("Logs available at:", build['logs']['deepLink'])
+    print("You can also follow the logs on the terminal:")
+    print()
+    print(f"    aws logs tail --follow {build['logs']['groupName']}")
+    print()
 
 
 if __name__ == "__main__":
