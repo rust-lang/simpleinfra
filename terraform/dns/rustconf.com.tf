98cefee6746d2a32ff965f11230989743ee0459d
Pietro Albini <pietro@pietroalbini.org>
2020-11-24 19:31:05 +0100

    terraform: configure email for rustconf.com

--- terraform/dns/rustconf.com.tf
+++ terraform/dns/rustconf.com.tf
@@ -21,4 +21,13 @@ module "rustconf_com" {
     "2019" = ["tildeio.github.io"],
     "cfp"  = ["cfp.rustconf.com.herokudns.com"],
   }
+
+  MX = {
+    "@" = local.mailgun_mx,
+  }
+
+  TXT = {
+    "@"             = [local.mailgun_spf],
+    "k1._domainkey" = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6AVUUbCl/Ofx/ceeWryb4v//f/Sr5UGrRt4ibA4UY2JQ3Db++KEBsdur5qAxC9eiWeAbYQyfQhFGagbmLjyxd1eNepzAIPesz81U0hodsLCaKs7OHkLrQJcTRxVYwucL5rBXVLdAhg7A1veVeN1xiqh8HmEull5vYsmOknf6mtwIDAQAB"],
+  }
 }
