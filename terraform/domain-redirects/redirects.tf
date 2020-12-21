2093ba2d3add5cd85e66d7ece4689e9fedba888d
Pietro Albini <pietro@pietroalbini.org>
2020-12-21 11:17:51 +0100

    terraform: add the package.metadata.docs.rs redirect

--- terraform/domain-redirects/redirects.tf
+++ terraform/domain-redirects/redirects.tf
@@ -55,3 +55,17 @@ module "arewewebyet_org" {
     "arewewebyet.org"
   ]
 }
+
+// https://github.com/rust-lang/docs.rs/issues/1210
+module "docs_rs_metadata" {
+  source = "./impl"
+  providers = {
+    aws       = aws
+    aws.east1 = aws.east1
+  }
+
+  to = "https://docs.rs/about/metadata"
+  from = [
+    "package.metadata.docs.rs",
+  ]
+}
