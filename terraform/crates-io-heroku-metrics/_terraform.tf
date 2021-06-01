1f3bf14a5c1cd3b8e294b7fbf75c29983ca5a188
Pietro Albini <pietro@pietroalbini.org>
2021-06-01 13:50:06 +0200

    configure crates-io-heroku-metrics

---
+++ terraform/crates-io-heroku-metrics/_terraform.tf
@@ -0,0 +1,38 @@
+// Configuration for Terraform itself.
+
+terraform {
+  required_version = "~> 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+    github = {
+      source  = "hashicorp/github"
+      version = "~> 2.9.2"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "rust-terraform"
+    key            = "simpleinfra/crates-io-heroku-metrics.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+data "terraform_remote_state" "shared" {
+  backend = "s3"
+  config = {
+    bucket = "rust-terraform"
+    key    = "simpleinfra/shared.tfstate"
+    region = "us-west-1"
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
---
+++ terraform/crates-io-heroku-metrics/app.tf
@@ -0,0 +1,29 @@
+// ECS deployment and CI integration of crates-io-heroku-metrics.
+
+module "crates_io_heroku_metrics" {
+  source         = "../shared/modules/ecs-app"
+  cluster_config = data.terraform_remote_state.shared.outputs.ecs_cluster_config
+
+  env  = "prod"
+  name = "crates-io-heroku-metrics"
+  repo = "rust-lang/crates-io-heroku-metrics"
+
+  cpu              = 512
+  memory           = 1024
+  tasks_count      = 1
+  platform_version = "1.4.0"
+
+  secrets = {
+    PASSWORD_DRAIN   = "/prod/crates-io-heroku-metrics/password-drain"
+    PASSWORD_METRICS = "/prod/crates-io-heroku-metrics/password-metrics"
+  }
+
+  expose_http = {
+    container_port = 80
+    domains        = ["crates-io-heroku-metrics.infra.rust-lang.org"]
+
+    health_check_path     = "/health"
+    health_check_interval = 60
+    health_check_timeout  = 15
+  }
+}
