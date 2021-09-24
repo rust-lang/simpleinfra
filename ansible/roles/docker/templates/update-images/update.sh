7c433d64069908f2430e9e37e84b181c8c12eaca
Mark Rousskov <mark.simulacrum@gmail.com>
2021-09-24 18:43:34 -0400

    Retry the docker update if it doesn't succeed
    We're seeing a lot of spurious timeouts on crater-azure-{1,2}, not clear what
    the cause is, but hopefully can be worked around by retrying.

--- ansible/roles/docker/templates/update-images/update.sh
+++ ansible/roles/docker/templates/update-images/update.sh
@@ -6,12 +6,32 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# See https://unix.stackexchange.com/questions/82598
+# Copied from rust-lang/rust's CI
+function retry {
+  echo "Attempting with retry:" "$@"
+  local n=1
+  local max=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        sleep $n  # don't retry immediately
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+      else
+        echo "The command has failed after $n attempts."
+        return 1
+      fi
+    }
+  done
+}
+
 update_image() {
     container="$1"
     image="$2"
 
     old_id="$(docker images --format "{{ '{{.ID}}' }}" "${image}")"
-    docker pull "${image}"
+    retry docker pull "${image}"
     new_id="$(docker images --format "{{ '{{.ID}}' }}" "${image}")"
 
     if [[ "${old_id}" != "${new_id}" ]] && [[ -z "${NO_CONTAINER_RESTART+x}" ]]; then
