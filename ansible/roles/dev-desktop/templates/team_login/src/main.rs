963280b0c1698688bdbccaeedabad783f01e2f13
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2021-11-16 15:37:51 +0000

    Run rustfmt

--- ansible/roles/dev-desktop/templates/team_login/src/main.rs
+++ ansible/roles/dev-desktop/templates/team_login/src/main.rs
@@ -1,10 +1,10 @@
 const TEAM_URL: &str = "https://team-api.infra.rust-lang.org/v1/teams/all.json";
 
-use serde::Deserialize;
 use eyre::*;
+use serde::Deserialize;
+use std::collections::HashSet;
 use std::process::Command;
 use std::process::Output;
-use std::collections::HashSet;
 
 #[derive(Deserialize)]
 struct All {
@@ -17,23 +17,25 @@ struct Person {
 }
 
 fn cmd(cmd: &str, args: &[&str]) -> std::io::Result<Output> {
-    Command::new(cmd)
-        .args(args)
-        .output()
+    Command::new(cmd).args(args).output()
 }
 
 const KEY_DIR: &str = "/etc/ssh/authorized_keys/";
 
 fn main() -> Result<()> {
-    let all = reqwest::blocking::get(TEAM_URL)?.json::<All>().wrap_err("failed to fetch team repository")?;
+    let all = reqwest::blocking::get(TEAM_URL)?
+        .json::<All>()
+        .wrap_err("failed to fetch team repository")?;
     let mut users = HashSet::new();
     for person in all.members {
         users.insert(person.github.clone());
         // Get the keys the user added to their github account.
         // Do this every time the cronjob runs so that they get their new keys if necessary.
-        let keys =
-            reqwest::blocking::get(format!("https://github.com/{}.keys", person.github)).wrap_err("failed to download user's keys")?.text()?;
-        std::fs::write(format!("{}{}", KEY_DIR, person.github), keys).wrap_err("Failed to create key file")?;
+        let keys = reqwest::blocking::get(format!("https://github.com/{}.keys", person.github))
+            .wrap_err("failed to download user's keys")?
+            .text()?;
+        std::fs::write(format!("{}{}", KEY_DIR, person.github), keys)
+            .wrap_err("Failed to create key file")?;
 
         // Check if user exists
         let id = cmd("id", &[&person.github]).wrap_err("failed to run `id` command")?;
@@ -43,13 +45,17 @@ fn main() -> Result<()> {
 
         // If user does not exist, create it
         ensure!(
-            cmd("useradd", &["--create-home", &person.github])?.status.success(),
+            cmd("useradd", &["--create-home", &person.github])?
+                .status
+                .success(),
             "failed to create user"
         );
 
         // Get them ssh access
         ensure!(
-            cmd("usermod", &["-a", "-G", "allow-ssh", &person.github])?.status.success(),
+            cmd("usermod", &["-a", "-G", "allow-ssh", &person.github])?
+                .status
+                .success(),
             "failed to give user ssh access"
         );
     }
