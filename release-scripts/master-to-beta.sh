58899808733c05e9f66e2f696d98c7e70fcd04c1
Pietro Albini <pietro.albini@ferrous-systems.com>
2022-01-10 17:12:39 +0100

    push the cargo commit from the cargo repository

--- release-scripts/master-to-beta.sh
+++ release-scripts/master-to-beta.sh
@@ -12,7 +12,7 @@ CURRENT_MASTER=`git ls-remote -q git@github.com:rust-lang/rust master | awk '{ p
 BRANCH_POINT=`git log --merges --first-parent --format="%P" -1 $CURRENT_MASTER -- src/version | awk '{print($1)}'`
 NEW_BETA_VERSION=`git show $BRANCH_POINT:src/version`
 CARGO_SHA=`git rev-parse $BRANCH_POINT:src/tools/cargo`
-git push git@github.com:rust-lang/cargo $CARGO_SHA:refs/heads/rust-$NEW_BETA_VERSION
+git -C src/tools/cargo push git@github.com:rust-lang/cargo $CARGO_SHA:refs/heads/rust-$NEW_BETA_VERSION
 
 echo "Disable branch protection for beta on rust-lang/rust, then press enter."
 read
