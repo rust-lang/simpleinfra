e0fd4366fe50d49856dc4ed60642eabf44d58f00
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-03-17 17:32:21 +0000

    Just overwrite the entire motd

--- ansible/roles/dev-desktop/templates/motd_rules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Continued use of this server implies that you accept the rules."
-echo "You can find the rules at"
-echo "https://foundation.rust-lang.org/policies/cloud-compute-program/"
