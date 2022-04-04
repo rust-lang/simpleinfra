d4546d1a59c68de9e88e202897710237593ffbe2
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-04-04 09:39:35 +0000

    Grab the github username by stripping the `gh-` prefix

--- ansible/roles/dev-desktop/files/scripts/init.sh
+++ ansible/roles/dev-desktop/files/scripts/init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:`id -u -n`/rust.git
+username=`id -u -n`
+gh_name=${username#"gh-"}
+
+git clone git@github.com:$gh_name/rust.git
 pushd rust
 git remote add upstream git@github.com:rust-lang/rust.git
 git fetch upstream
