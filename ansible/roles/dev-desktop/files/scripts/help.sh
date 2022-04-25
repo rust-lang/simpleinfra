96c8fc5bbec9f2c7d0f79f8655136c5f4ecf527e
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-04-25 15:34:56 +0000

    Add explainer for all the scripts

---
+++ ansible/roles/dev-desktop/files/scripts/help.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "# Available scripts for managing your Rust checkouts"
+echo "init.sh              | first time setup, you should only have to execute this once on a new machine"
+echo "status.sh            | list the branches and git status of all copies of the Rust repo"
+echo "new_worktree.sh      | creates a worktree (shallow copy of the main git checkout of Rust, sharing the .git folder)"
+echo "detach_merged_prs.sh | invokes `git pull --fast-forward-only` on all worktrees and detaches those that are equal to the `master` branch"
+echo ""
+echo "# Rarer commands:"
+echo "set_defaults.sh      | connects the global config.toml with all worktrees. Use this when your setup is broken" 
