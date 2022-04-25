6e5ef9b5bb616ba7cd75a818aed68a7c334aa8d7
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-04-25 15:06:19 +0000

    Set the users' default shell to bash

--- ansible/roles/dev-desktop/files/team_login/src/main.rs
+++ ansible/roles/dev-desktop/files/team_login/src/main.rs
@@ -82,6 +82,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .success(),
             "failed to give user ssh access"
         );
+
+        // Set their default shell
+        assert!(
+            cmd("usermod", &["--shell", "/usr/bin/bash", &username])?
+                .status
+                .success(),
+            "failed to set the default shell"
+        );
     }
     // Delete all keys for users that weren't on the list
     for entry in std::fs::read_dir(KEY_DIR)? {
