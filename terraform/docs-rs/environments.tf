68083851ca3d257d8ceacb47b7fb770597226370
Pietro Albini <pietro@pietroalbini.org>
2022-07-02 18:53:21 +0200

    remove unused staging environment

--- terraform/docs-rs/environments.tf
+++
@@ -1,8 +0,0 @@
-module "staging" {
-  source = "./impl"
-
-  env_name = "staging"
-
-  ci_username            = aws_iam_user.ci.name
-  inventories_bucket_arn = data.terraform_remote_state.shared.outputs.inventories_bucket_arn
-}
