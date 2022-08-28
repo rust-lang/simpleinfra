3b055547fd37f42bee53e7c2c8c9800c013a93c9
Mark Rousskov <mark.simulacrum@gmail.com>
2022-08-28 13:04:42 -0400

    Move promote-release CI to OIDC

--- terraform/releases/promote-release-ci.tf
+++ terraform/releases/promote-release-ci.tf
@@ -7,19 +7,34 @@ module "promote_release_ecr" {
 
 // IAM User used by CI to push the built images on ECR.
 
-module "promote_release_iam_ci" {
-  source = "../shared/modules/gha-iam-user"
-  org    = "rust-lang"
-  repo   = "promote-release"
+resource "aws_iam_role" "ci_promote_release" {
+  name = "ci--rust-lang-promote-release"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = data.terraform_remote_state.shared.outputs.gha_oidc_arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:rust-lang/promote-release:ref:refs/heads/master"
+          }
+        }
+      }
+    ]
+  })
 }
 
-
-resource "aws_iam_user_policy_attachment" "promote_release_ci_pull" {
-  user       = module.promote_release_iam_ci.user_name
+resource "aws_iam_role_policy_attachment" "promote_release_ci_pull" {
+  role       = aws_iam_role.ci_promote_release.name
   policy_arn = module.promote_release_ecr.policy_pull_arn
 }
 
-resource "aws_iam_user_policy_attachment" "promote_release_ci_push" {
-  user       = module.promote_release_iam_ci.user_name
+resource "aws_iam_role_policy_attachment" "promote_release_ci_push" {
+  role       = aws_iam_role.ci_promote_release.name
   policy_arn = module.promote_release_ecr.policy_push_arn
 }
