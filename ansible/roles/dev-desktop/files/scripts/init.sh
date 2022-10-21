d52d8bb5241f1f4a6b633a0c1d11744ff6c5e63e
Jan David <jdno@jdno.dev>
2022-10-21 16:21:20 +0200

    Restore original name for init script

--- ansible/roles/dev-desktop/files/scripts/init.sh
+++ ansible/roles/dev-desktop/files/scripts/init.sh
@@ -5,13 +5,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Check if rustup is already installed and exit if that's the case.
-if command -v rustup &>/dev/null; then
-  rustup --version
-  exit 0
-fi
-
 init_git.py
-
-echo "Installing rustup..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+setup_rustup.sh
---
+++ ansible/roles/dev-desktop/files/scripts/setup_rustup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Enable strict mode for Bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Check if rustup is already installed and exit if that's the case.
+if command -v rustup &>/dev/null; then
+  rustup --version
+  exit 0
+fi
+
+echo "Installing rustup..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
