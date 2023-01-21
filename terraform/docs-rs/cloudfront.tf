1b05fce8487e91b08883de21baf1c9b6ff23181b
Denis Cornehl <denis@cornehl.org>
2023-01-21 08:36:36 +0100

    increase cloudfront TTL for docs.rs and static.docs.rs

--- terraform/docs-rs/cloudfront.tf
+++ terraform/docs-rs/cloudfront.tf
@@ -41,7 +41,7 @@ resource "aws_cloudfront_distribution" "webapp" {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
 
-    default_ttl = 900 // 15 minutes
+    default_ttl = 86400 // 1 day
     min_ttl     = 0
     max_ttl     = 31536000 // 1 year
 
