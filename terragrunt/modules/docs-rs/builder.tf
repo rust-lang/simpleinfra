beae5dbdcc96f63cab0adb51518a061a4bdac8e9
Ryan Levick <me@ryanlevick.com>
2023-02-07 11:21:13 +0100

    Get AWS communication working

---
+++ terragrunt/modules/docs-rs/builder.tf
@@ -0,0 +1,44 @@
+// The instance profile the builder will assume when communicating with
+// other AWS services.
+
+resource "aws_iam_instance_profile" "builder" {
+  name = "builder"
+  role = aws_iam_role.builder.name
+}
+
+resource "aws_iam_role" "builder" {
+  name = "builder"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = ["sts:AssumeRole"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "builder_s3" {
+  role = aws_iam_role.builder.name
+  name = "builder_s3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      // Access to s3
+      {
+        Effect = "Allow"
+        Action = "s3:*"
+
+        Resource = [
+          aws_s3_bucket.storage.arn,
+          "${aws_s3_bucket.storage.arn}/*"
+        ]
+      }
+    ]
+  })
+}
