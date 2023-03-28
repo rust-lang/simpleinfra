3f0a8dbdc8dc5f9304a8c8021633a6cd29edbb88
Jan David <jdno@jdno.dev>
2023-03-28 15:22:49 +0200

    Fix format of Terraform files

--- terraform/rust-log-analyzer/_terraform.tf
+++ terraform/rust-log-analyzer/_terraform.tf
@@ -33,10 +33,10 @@ data "terraform_remote_state" "shared" {
 }
 
 provider "aws" {
-  region  = "us-west-1"
+  region = "us-west-1"
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  alias   = "east1"
+  region = "us-east-1"
+  alias  = "east1"
 }
