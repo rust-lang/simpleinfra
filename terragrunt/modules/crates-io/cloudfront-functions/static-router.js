2c6252ebf179363f4dc76019ba8b798f88deb639
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-11 21:55:38 +0100

    crates-io/cloudfront-functions: Add support for download URLs without placeholders

--- terragrunt/modules/crates-io/cloudfront-functions/static-router.js
+++ terragrunt/modules/crates-io/cloudfront-functions/static-router.js
@@ -7,5 +7,15 @@ function handler(event) {
         request.uri = request.uri.replace("+", "%2B");
     }
 
+    // cargo versions before 1.24 don't support placeholders in the `dl` field
+    // of the index, so we need to rewrite the download URL to point to the
+    // crate file instead.
+    var match = request.uri.match(/^\/crates\/([^\/]+)\/([^\/]+)\/download$/);
+    if (match) {
+        var crate = match[1];
+        var version = match[2];
+        request.uri = `/crates/${crate}/${crate}-${version}.crate`;
+    }
+
     return request;
 }
