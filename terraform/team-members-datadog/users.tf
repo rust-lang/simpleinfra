509508ca4bd911bd95270d10078fac41f9c3ff01
Jan David <jdno@jdno.dev>
2023-12-12 14:49:49 +0100

    Add board members

--- terraform/team-members-datadog/users.tf
+++ terraform/team-members-datadog/users.tf
@@ -20,10 +20,20 @@ locals {
       name  = "Mark Rousskov"
       roles = ["DataDog Admin Role"]
     }
+    "nell" = {
+      login = "nells@microsoft.com"
+      name  = "Nell Shamrell-Harrington"
+      roles = ["Board Member"]
+    }
     "paullenz" = {
       login = "paullenz@rustfoundation.org"
       name  = "Paul Lenz"
       roles = ["Datadog Read Only Role"]
+    }
+    "peixin" = {
+      login = "peixin.hou@gmail.com"
+      name  = "Peixin Hou"
+      roles = ["Board Member"]
     }
     "pietro" = {
       login = "pietro@pietroalbini.org"
@@ -34,6 +44,11 @@ locals {
       login = "infra@rustfoundation.org"
       name  = "Rust Foundation Infrastructure"
       roles = ["Datadog Admin Role"]
+    }
+    "seth" = {
+      login = "smarkle.aws@gmail.com"
+      name  = "Seth Markle"
+      roles = ["Board Member"]
     }
     "tobias" = {
       login = "tobiasbieniek@rustfoundation.org"
