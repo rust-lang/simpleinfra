4a29baf8babb44ae1567c533d1c92b81447a1572
Jan David <jdno@jdno.dev>
2024-02-12 13:39:45 +0100

    Restore access to logs for crates.io team

--- terraform/team-members-datadog/crates-io.tf
+++ terraform/team-members-datadog/crates-io.tf
@@ -12,6 +12,7 @@ resource "datadog_role" "crates_io" {
 
   dynamic "permission" {
     for_each = toset([
+      data.datadog_permissions.all.permissions.logs_read_index_data,
       data.datadog_permissions.all.permissions.logs_read_data,
       data.datadog_permissions.all.permissions.logs_live_tail,
       data.datadog_permissions.all.permissions.logs_read_archives,
