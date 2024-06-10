4042268225b919c5146d53c18d8fc37e08e5eb39
Jan David <jdno@jdno.dev>
2024-06-10 13:25:19 +0200

    Move Terraform providers into account.json
    Due to how Terragrunt generates the provider configuration, we use the
    account.json file to declare additional providers for Terraform. The
    existing providers for the crates.io module have been moved from
    Terraform to that file to avoid a duplicate provider error when running
    Terraform.

--- terragrunt/modules/crates-io/_terraform.tf
+++ terragrunt/modules/crates-io/_terraform.tf
@@ -12,16 +12,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "eu-west-1"
-  region = "eu-west-1"
-}
-
 provider "fastly" {}
 
 data "aws_caller_identity" "current" {}
