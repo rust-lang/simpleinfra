ba9bafee81f9ebfe759238c067400e21feafd58a
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-08-27 15:49:29 +0200

    chore: update `terraform/shared` to aws provider 5.64

--- terraform/shared/services/ecs-cluster/main.tf
+++ terraform/shared/services/ecs-cluster/main.tf
@@ -14,8 +14,7 @@ locals {
 }
 
 resource "aws_ecs_cluster" "cluster" {
-  name               = var.cluster_name
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+  name = var.cluster_name
 
   setting {
     name  = "containerInsights"
