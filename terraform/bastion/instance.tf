10abf28acad8f10bf01f062acddf34736e90c113
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-10-01 12:25:33 +0200

    assign `bastion` domain to ubuntu 24 ec2

--- terraform/bastion/instance.tf
+++ terraform/bastion/instance.tf
@@ -52,7 +52,7 @@ resource "aws_route53_record" "bastion" {
 
 resource "aws_route53_record" "bastion2" {
   zone_id = data.aws_route53_zone.rust_lang_org.id
-  name    = "bastion2.infra.rust-lang.org"
+  name    = "bastion.infra.rust-lang.org"
   type    = "A"
   records = [aws_eip.bastion.public_ip]
   ttl     = 300
