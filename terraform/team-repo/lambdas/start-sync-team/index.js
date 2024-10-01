94d8954cbadafd979a9cc95c3a7802fba66b98ec
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-10-01 14:50:28 +0200

    chore(team-repo-ci): update to nodejs runtime 20

--- terraform/team-repo/lambdas/start-sync-team/index.js
+++ terraform/team-repo/lambdas/start-sync-team/index.js
@@ -1,8 +1,9 @@
-const AWS = require('aws-sdk');
-const CodeBuild = new AWS.CodeBuild();
+const { CodeBuildClient, StartBuildCommand } = require("@aws-sdk/client-codebuild");
+
+const client = new CodeBuildClient();
 
 exports.handler = async function(event) {
-    return CodeBuild.startBuild({
+    await client.send(new StartBuildCommand({
         projectName: 'sync-team',
-    }).promise();
+    }));
 };
