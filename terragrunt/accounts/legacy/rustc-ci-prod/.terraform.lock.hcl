01ef81ab28e8ec04de91bd06eaa4f63a59f06337
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-02-11 12:08:53 +0100

    provision aws tokens for rust-lang/rust

---
+++ terragrunt/accounts/legacy/rustc-ci-prod/.terraform.lock.hcl
@@ -0,0 +1,25 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.86.0"
+  constraints = "~> 5.86"
+  hashes = [
+    "h1:UpRjlmF+Xi6q4YxMMlN/66sCI81VWJOKiumk1cfBptQ=",
+    "zh:1587c6a0199dc33d066c13e1628bc0dd966d7d6740cb2007b636524a3ec99430",
+    "zh:15af46cc5bb43a37c24438cb3a36d44209a89d923ea4d4d631b56b1a89717b26",
+    "zh:166902101ac1cc8ec4f53e3bdcbab2eac7eb448b1c428c2e622adbf9ce1a679c",
+    "zh:284d116ac9d4a4de74cd1f52486f00e10bc400d9654f92a8990ea0093c43ff78",
+    "zh:4135e928f20d456172c8ab4ae3d4d8e411b6feddc94aaa1347c92469d52f1e61",
+    "zh:72b317d17182c3e0ee72f2851d25565d369cb6ee803b12adc9b6c6d3dbfca8d7",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9dd0e80964e215ff658b708be72ccda8a20f63af7eaebdd6f11eb0461633bb03",
+    "zh:a18e502c16b7b6b216b888eab9a5c66b1ed103847fce6985850e4fc9e364a3e8",
+    "zh:c239f12648d7f7bbadbf5db0b57aaa9429abe70b574975b581784b4f17b7ed79",
+    "zh:c5164ca8254b9973ee985a3841a4b1f776844c7dcbc112ab3a88a0096e7e2198",
+    "zh:d93ac58092c3fffc5ddc688b39721fbfacc353e8965001060a5a1ce934d97246",
+    "zh:e877f1be2ebe67a2d163b7488f47cff4c95aca9c541ddfa25ad16c6ecc98f6a8",
+    "zh:eb71af6dfdd2b5670b5b957397a576d6053587c75750c17acc105fb44ed806eb",
+    "zh:ff6aa4f88f8e789375391bc8c886c636fb3e4a45a3fd7dc291bca17c2b8d4184",
+  ]
+}
---
+++ terragrunt/modules/rustc-ci/artifacts.tf
@@ -0,0 +1,190 @@
+locals {
+  bucket_name = "rust-lang-ci2"
+
+  rustc_builds     = "rustc-builds"
+  rustc_builds_alt = "rustc-builds-alt"
+
+  s3_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ArtifactsBucketWrite"
+        Effect = "Allow"
+        Resource = [
+          "${aws_s3_bucket.artifacts.arn}/${local.rustc_builds}",
+          "${aws_s3_bucket.artifacts.arn}/${local.rustc_builds}/*",
+          "${aws_s3_bucket.artifacts.arn}/${local.rustc_builds_alt}",
+          "${aws_s3_bucket.artifacts.arn}/${local.rustc_builds_alt}/*",
+
+        ]
+        Action = [
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+        ]
+      },
+      {
+        Sid      = "ArtifactsBucketList"
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.artifacts.arn}"
+        Action = [
+          "s3:ListBucket",
+        ],
+      },
+      {
+        Sid      = "HeadBuckets",
+        Effect   = "Allow",
+        Resource = "*"
+        Action = [
+          "s3:HeadBucket",
+          "s3:GetBucketLocation",
+        ],
+      },
+    ]
+  })
+}
+
+import {
+  to = aws_s3_bucket.artifacts
+  # id retrieved with `terraform state show`.
+  id = local.bucket_name
+}
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket = local.bucket_name
+
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    status = "Enabled"
+    id     = "cleanup-${local.rustc_builds}"
+
+    // Note that this applies equally to rustc-builds and rustc-builds-alt, as
+    // it is a prefix.
+    filter {
+      prefix = local.rustc_builds
+    }
+
+    expiration {
+      days = 168
+    }
+
+    noncurrent_version_expiration {
+      // This is *in addition* to the delete_artifacts_after_days above; we
+      // don't really need to keep CI artifacts around in an inaccessible state.
+      noncurrent_days = 1
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 1
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Principal = {
+          AWS = "*"
+        }
+        Sid      = "PublicReadGetObject"
+        Effect   = "Allow"
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.artifacts.arn}/*"
+      },
+    ]
+  })
+}
+
+module "artifacts_user" {
+  source = "../gha-iam-user"
+
+  org  = "rust-lang"
+  repo = "rust"
+
+  user_name  = "rustc-ci--rust-lang--rust--artifacts"
+  env_prefix = "ARTIFACTS"
+}
+
+resource "aws_iam_user_policy" "artifacts_write" {
+  name = "artifacts-write"
+  user = module.artifacts_user.user_name
+
+  policy = local.s3_policy
+}
+
+resource "aws_s3_bucket_acl" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+  acl    = "public-read"
+}
+
+module "artifacts_cdn" {
+  source = "../../shared/modules/static-website"
+  providers = {
+    aws = aws.east1
+  }
+
+  domain_name        = "ci-artifacts.rust-lang.org"
+  origin_domain_name = aws_s3_bucket.artifacts.bucket_regional_domain_name
+  response_policy_id = var.response_policy_id
+}
+
+data "aws_s3_bucket" "inventories" {
+  bucket = "rust-inventories"
+}
+
+resource "aws_s3_bucket_inventory" "artifacts" {
+  name    = "all-objects-csv"
+  bucket  = aws_s3_bucket.artifacts.id
+  enabled = true
+
+  included_object_versions = "Current"
+  optional_fields          = ["ETag", "Size", "StorageClass", "IntelligentTieringAccessTier"]
+
+  schedule {
+    frequency = "Weekly"
+  }
+  destination {
+    bucket {
+      bucket_arn = data.aws_s3_bucket.inventories.arn
+      prefix     = aws_s3_bucket.artifacts.id
+      format     = "CSV"
+    }
+  }
+}
+
+resource "aws_iam_role" "try_builds" {
+  name = "rustc-ci--rust-lang--rust--try-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = "arn:aws:iam::890664054962:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:rust-lang/rust:ref:refs/heads/automation/bors/try"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "try_builds" {
+  name   = "put-objects"
+  role   = aws_iam_role.try_builds.id
+  policy = local.s3_policy
+}
