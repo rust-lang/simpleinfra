40519dc9110a1c354c7efbcaa08003a3f12886cf
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-08 15:35:08 +0200

    delete team-repo terraform module

--- terraform/team-repo/ci.tf
+++
@@ -1,8 +0,0 @@
-// Resources needed for the CI of rust-lang/team and rust-lang/sync-team.
-
-// ECR repo used to store the Docker image built by rust-lang/sync-team's CI.
-
-module "ecr" {
-  source = "../shared/modules/ecr-repo"
-  name   = "sync-team"
-}
