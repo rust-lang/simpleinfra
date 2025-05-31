12ca20bceed6b7ad3139331e961e464ca15d20d1
Pietro Albini <pietro@pietroalbini.org>
2025-05-31 11:28:34 +0200

    update protocol version

--- terragrunt/modules/gha-self-hosted-images/cdn.tf
+++ terragrunt/modules/gha-self-hosted-images/cdn.tf
@@ -14,7 +14,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     acm_certificate_arn      = module.certificate.arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.1_2016"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   default_cache_behavior {
