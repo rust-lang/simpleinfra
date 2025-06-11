a5dfc484c7f9cc87d838792c0079ff95082f3da4
Jan David <jdno@jdno.dev>
2025-06-11 14:58:21 +0200

    Create DNS records for rustc-perf servers

---
+++ terraform/rustc-perf/dns.tf
@@ -0,0 +1,19 @@
+data "aws_route53_zone" "rust_lang_org" {
+  name = "rust-lang.org"
+}
+
+resource "aws_route53_record" "legacy" {
+  zone_id = data.aws_route53_zone.rust_lang_org.id
+  name    = "rustc-perf-legacy.infra.rust-lang.org"
+  type    = "A"
+  records = ["159.69.58.186"]
+  ttl     = 300
+}
+
+resource "aws_route53_record" "one" {
+  zone_id = data.aws_route53_zone.rust_lang_org.id
+  name    = "rustc-perf-one.infra.rust-lang.org"
+  type    = "A"
+  records = ["144.76.186.39"]
+  ttl     = 300
+}
