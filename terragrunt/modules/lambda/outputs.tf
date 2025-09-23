f52f538b7ebe66ae1a4f85a34fd518a0861af553
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-09-23 16:11:09 +0200

    test s3-tsv with a cloudwatch event

---
+++ terragrunt/modules/lambda/outputs.tf
@@ -0,0 +1,9 @@
+output "function_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.this.arn
+}
+
+output "function_name" {
+  description = "Name of the Lambda function"
+  value       = aws_lambda_function.this.function_name
+}
