4dfa2ae68ccc212390572d21722efd63229ee420
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-09-25 11:37:48 +0200

    delete useless aws s3-tsv lambda

--- terragrunt/modules/lambda/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "function_arn" {
-  description = "ARN of the Lambda function"
-  value       = aws_lambda_function.this.arn
-}
-
-output "function_name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.this.function_name
-}
