d3ca602776f098e4de84d30125397f6b2dd67a0d
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-11-27 14:39:06 +0100

    allow docs-rs team to mute alerts

--- terraform/team-members-datadog/docs-rs.tf
+++ terraform/team-members-datadog/docs-rs.tf
@@ -18,6 +18,7 @@ resource "datadog_role" "docs_rs" {
       data.datadog_permissions.all.permissions.logs_read_archives,
       data.datadog_permissions.all.permissions.dashboards_write,
       data.datadog_permissions.all.permissions.monitors_write,
+      data.datadog_permissions.all.permissions.monitors_downtime,
       data.datadog_permissions.all.permissions.notebooks_write,
       data.datadog_permissions.all.permissions.dbm_read,
     ])
