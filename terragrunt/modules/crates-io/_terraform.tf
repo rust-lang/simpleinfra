67818c255661f5b09ce10f03d207d39e10db7fa3
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-12-04 12:54:07 +0100

    create index-crates-io fastly CDN

--- terragrunt/modules/crates-io/_terraform.tf
+++ terragrunt/modules/crates-io/_terraform.tf
@@ -82,6 +82,17 @@ variable "static_fastly_weight" {
   type        = number
 }
 
+# TODO: uncomment
+# variable "index_cloudfront_weight" {
+#   description = "Weight of the traffic for index.crates.io that is routed through CloudFront"
+#   type        = number
+# }
+
+variable "index_fastly_weight" {
+  description = "Weight of the traffic for index.crates.io that is routed through Fastly"
+  type        = number
+}
+
 variable "fastly_aws_account_id" {
   # See https://docs.fastly.com/en/guides/creating-an-aws-iam-role-for-fastly-logging
   description = "The AWS account ID that Fastly uses to write logs"
