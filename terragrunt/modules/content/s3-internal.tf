9fbb43946501997cac9d8f54c9024fc76ba0d700
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-01-26 14:38:18 +0100

    restrict content team internal bucket

--- terragrunt/modules/content/s3-internal.tf
+++ terragrunt/modules/content/s3-internal.tf
@@ -1,3 +1,19 @@
+# Bucket for private artifacts used by the content team.
+# E.g., raw podcast audio files that need to be edited before publishing.
 resource "aws_s3_bucket" "internal" {
   bucket = "rust-content-internal"
+}
+
+# Block all public access paths so S3 is not directly reachable.
+resource "aws_s3_bucket_public_access_block" "internal" {
+  bucket = aws_s3_bucket.internal.id
+
+  # Prevent public ACLs from granting access.
+  block_public_acls = true
+  # Prevent public bucket policies from granting access.
+  block_public_policy = true
+  # Ignore any public ACLs that might be applied.
+  ignore_public_acls = true
+  # Restrict public bucket policies.
+  restrict_public_buckets = true
 }
