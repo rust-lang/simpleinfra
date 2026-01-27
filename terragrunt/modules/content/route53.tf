a5c6fcf3ade906025168514878e4e2e0f3d039f7
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-01-27 15:38:36 +0100

    create content route53 ipv6 record

--- terragrunt/modules/content/route53.tf
+++ terragrunt/modules/content/route53.tf
@@ -20,3 +20,16 @@ resource "aws_route53_record" "public" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "public_ipv6" {
+  zone_id = aws_route53_zone.zone.id
+  name    = local.domain_name
+  type    = "AAAA"
+
+  # Use an alias record to point at the CloudFront distribution.
+  alias {
+    name                   = aws_cloudfront_distribution.public.domain_name
+    zone_id                = aws_cloudfront_distribution.public.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
