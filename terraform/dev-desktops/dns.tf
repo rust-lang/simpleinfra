89d468ef5be9d22aeb5ca39bc14139cdab8bb0aa
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-01-29 17:07:39 +0100

    add ipv6 route53 record for us-2 dev-desktop

--- terraform/dev-desktops/dns.tf
+++ terraform/dev-desktops/dns.tf
@@ -10,3 +10,12 @@ resource "aws_route53_record" "dev_desktop_us_2" {
   records = ["136.114.133.79"]
   ttl     = 60
 }
+
+resource "aws_route53_record" "dev_desktop_us_2_ipv6" {
+  zone_id = data.aws_route53_zone.rust_lang_org.id
+  name    = "dev-desktop-us-2.infra.rust-lang.org"
+  type    = "AAAA"
+  # IPv6 address of dev-desktop-us-2 in GCP
+  records = ["2600:1900:4000:4ed0:0:0:0:0"]
+  ttl     = 60
+}
