ff06d1721ec9e1d56cdf7cff29ce165571cb0686
Mark Rousskov <mark.simulacrum@gmail.com>
2026-08-01 13:07:57 -0400

    Build instance AMIs for gha-runner
    This sets up a cron job to rebuild daily and update an SSM parameter for
    the instances used for CI.

---
+++ terragrunt/modules/ci-runners/lambda/index.py
@@ -0,0 +1,109 @@
+import datetime
+import io
+import json
+import os
+import subprocess
+import uuid
+from hashlib import sha256
+from zipfile import ZipFile
+
+import boto3
+
+# Download and extract packer at initialization time
+# This is intentionally outside of the handler so this is cached if we get
+# invoked multiple times.
+packer_zip = subprocess.run(
+    [
+        "curl",
+        "https://releases.hashicorp.com/packer/1.16.0/packer_1.16.0_linux_arm64.zip",
+        "-o",
+        "-",
+    ],
+    capture_output=True,
+    check=True,
+)
+if (
+    sha256(packer_zip.stdout).hexdigest()
+    != "cf18f03460d92265d49b56befff333e80641d845822799eab04357c39f75b5d7"
+):
+    print("Stderr:", packer_zip.stderr)
+    print("Got:", sha256(packer_zip.stdout).hexdigest())
+    raise RuntimeError("Hash mismatch")
+with ZipFile(io.BytesIO(packer_zip.stdout)) as packer_zip:
+    packer_zip.extract("packer", path="/tmp/")
+os.chmod("/tmp/packer", 0o755)
+
+
+# TODO: Ideally we'd do some kind of verification the AMI works before we publish it to SSM
+# But since it's not super clear how to do that nicely and breakage is relatively unlikely,
+# for now we just directly update.
+def update_ami(ami_name):
+    # Now that we should have an AMI, we want to place it's ID in an SSM parameter.
+    # Getting the ID out of packer is annoying so just query EC2 for it from the name.
+    ec2 = boto3.client("ec2", region_name="us-east-2")
+    images = ec2.describe_images(Owners=["self"])
+    for image in images["Images"]:
+        creation_date = datetime.datetime.fromisoformat(image["CreationDate"])
+        age_in_days = (
+            (
+                datetime.datetime.now(datetime.UTC).timestamp()
+                - creation_date.timestamp()
+            )
+            / 3600
+            / 24
+        )
+        if image["Name"] == ami_name:
+            ssm = boto3.client("ssm", region_name="us-east-2")
+            ssm.put_parameter(
+                Name="latest-gha-runner-ami",
+                Value=image["ImageId"],
+                Type="String",
+                DataType="aws:ec2:image",
+                Overwrite=True,
+            )
+        elif age_in_days > 2:
+            ec2.deregister_image(
+                ImageId=image["ImageId"], DeleteAssociatedSnapshots=True
+            )
+
+
+def handler(event, context):
+    ami_name = "packer-gha-runner-" + str(uuid.uuid4())
+    latest_release = subprocess.run(
+        [
+            "curl",
+            "-H",
+            "Accept: application/vnd.github+json",
+            "-H",
+            "X-GitHub-Api-Version: 2026-03-10",
+            "https://api.github.com/repos/actions/runner/releases/latest",
+            "-o",
+            "-",
+        ],
+        capture_output=True,
+        check=True,
+    )
+    runner_url = None
+    for asset in json.loads(latest_release.stdout)["assets"]:
+        url = asset["browser_download_url"]
+        if "linux-x64" in url:
+            runner_url = url
+    # Then execute the packer build
+    with_home = os.environ.copy()
+    with_home["HOME"] = "/tmp"
+    subprocess.run(["/tmp/packer", "init", "ubuntu.pkr.hcl"], check=True, env=with_home)
+    subprocess.run(
+        [
+            "/tmp/packer",
+            "build",
+            "-var",
+            f"ami_name={ami_name}",
+            "-var",
+            f"runner_url={runner_url}",
+            "ubuntu.pkr.hcl",
+        ],
+        check=True,
+        env=with_home,
+    )
+
+    update_ami(ami_name)
---
+++ terragrunt/modules/ci-runners/rebuild.tf
@@ -0,0 +1,173 @@
+resource "aws_lambda_function" "image_builder" {
+  source_code_hash = data.external.image_builder.result.base64sha256
+  filename         = data.external.image_builder.result.path
+
+  function_name = "gha-image-builder"
+  role          = aws_iam_role.image_builder.arn
+  architectures = ["arm64"]
+
+  handler      = "index.handler"
+  runtime      = "python3.13"
+  package_type = "Zip"
+  timeout      = 900
+  memory_size  = 1024
+
+  depends_on = [aws_cloudwatch_log_group.image_builder]
+}
+
+resource "aws_lambda_function_event_invoke_config" "image_builder" {
+  function_name = aws_lambda_function.image_builder.function_name
+
+  maximum_event_age_in_seconds = 120
+  # Don't retry if AMI building fails.
+  maximum_retry_attempts = 0
+}
+
+resource "aws_cloudwatch_log_group" "image_builder" {
+  name              = "/aws/lambda/gha-image-builder"
+  retention_in_days = 90
+}
+
+data "external" "image_builder" {
+  program = ["${path.module}/../../modules/aws-lambda/pack.py"]
+  query = {
+    source_dir  = "${path.module}/lambda"
+    destination = "/tmp/image-builder.zip"
+  }
+}
+
+resource "aws_iam_role" "image_builder" {
+  name = "image-builder"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "image_builder" {
+  name = "image-builder"
+  role = aws_iam_role.image_builder.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Resource = [
+          "*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:Describe*",
+          "ec2:RunInstances",
+          "ec2:StopInstances",
+          "ec2:TerminateInstances",
+          "ec2:CreateKeyPair",
+          "ec2:DeleteKeyPair",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:CreateImage",
+          "ec2:DeregisterImage",
+          "ebs:DeleteSnapshot",
+        ]
+        Resource = [
+          "*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:PutParameter",
+        ]
+        Resource = [
+          "*",
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_scheduler_schedule" "image_builder" {
+  name       = "daily-rebuild"
+  group_name = "default"
+
+  flexible_time_window {
+    mode                      = "FLEXIBLE"
+    maximum_window_in_minutes = 60
+  }
+
+  schedule_expression = "rate(1 days)"
+
+  target {
+    arn      = aws_lambda_function.image_builder.arn
+    role_arn = aws_iam_role.image_invoker.arn
+
+    # No input needed
+    input = jsonencode({})
+
+    # Don't retry more than once. Building AMIs is relatively expensive, we don't
+    # need to accidentally spin up a bunch of builders.
+    retry_policy {
+      maximum_retry_attempts = 1
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "image_invoker" {
+  name = "image-invoker"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = "${data.aws_caller_identity.current.account_id}"
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "image_invoker" {
+  name = "image-invoker"
+  role = aws_iam_role.image_invoker.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Resource = [
+          aws_lambda_function.image_builder.arn,
+        ]
+      }
+    ]
+  })
+}
