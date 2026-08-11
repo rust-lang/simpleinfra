0c9a4ffd5d0b6ec12cedf5a4a403351437b2aee2
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-08-11 19:06:24 +0200

    Enable CloudTrail trusted access in AWS Organizations

--- terragrunt/modules/aws-organization/accounts.tf
+++ terragrunt/modules/aws-organization/accounts.tf
@@ -1,5 +1,8 @@
 resource "aws_organizations_organization" "rust" {
-  aws_service_access_principals = ["sso.amazonaws.com"]
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "sso.amazonaws.com",
+  ]
 }
 
 resource "aws_organizations_account" "admin" {
