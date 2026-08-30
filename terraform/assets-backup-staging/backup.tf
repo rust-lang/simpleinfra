a19276ae60725aaa33295b77e572eb6ee67cb15d
Pezhman Zandi <56797886+begininvoke@users.noreply.github.com>
2026-08-30 05:50:28 +0330

    fix(security): Artifact: terraform/assets-backup-staging/backup.tf Type: Secret AWS Access Key ID Severity: CRITICAL Ma

--- terraform/assets-backup-staging/backup.tf
+++ terraform/assets-backup-staging/backup.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key_id_crates_io" {
+  description = "AWS access key ID for crates-io bucket"
+  type        = string
+}
+
+variable "aws_access_key_id_static_rust_lang_org" {
+  description = "AWS access key ID for static‑rust‑lang‑org bucket"
+  type        = string
+}
+
 module "backup" {
   source     = "../shared/modules/assets-backup"
   project_id = "concrete-racer-468119-m7"
@@ -14,14 +24,14 @@ module "backup" {
       # cloudfront-static.staging.crates.io
       cloudfront_id     = "d23cyymnjtuccx"
       description       = "Staging crates for testing"
-      aws_access_key_id = "AKIA46X5W6CZBSN3RGGN"
+      aws_access_key_id = var.aws_access_key_id_crates_io
     }
     static-rust-lang-org = {
       bucket_name = "dev-static-rust-lang-org"
       # cloudfront-dev-static.rust-lang.org
       cloudfront_id     = "d29bglnmyg6h72"
       description       = "Development Rust releases"
-      aws_access_key_id = "AKIA46X5W6CZC6PEZ36Z"
+      aws_access_key_id = var.aws_access_key_id_static_rust_lang_org
     }
   }
 }
